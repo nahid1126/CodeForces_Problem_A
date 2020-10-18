bf67fbdd05882d3bda137b84fdb905a31920c09c
Nahid Hasan <nahidnub1126@gmail.com>
2020-10-18 20:57:30 +0600

    another problem in codeforces

---
+++ A.Fence.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    int n,sum=0;
+    cin>>n;
+    while(n--)
+    {
+        int a,b,c;
+        cin>>a>>b>>c;
+
+        cout<<((a+b+c)-1)<<'\n';
+    }
+}
+
