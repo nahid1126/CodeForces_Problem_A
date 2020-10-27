0815c27034e84af86092c246173ae5caa43f7210
Nahid Hasan <nahidnub1126@gmail.com>
2020-10-27 23:53:27 +0600

    another problem

---
+++ A.Marketing_Scheme.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+void solve()
+{
+    int l,r;
+    cin>>l>>r;
+    cout<<(l*2>r?"YES\n":"NO\n");
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        solve();
+
+    }
+    return 0;
+}
